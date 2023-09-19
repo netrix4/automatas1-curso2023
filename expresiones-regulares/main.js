931905c580bb481ec3831231489965b09e92b564
Mario Arias <netrix4@gmail.com>
2023-09-19 01:03:39 -0700

    completed-second-subject-test

--- expresiones-regulares/main.js
+++ expresiones-regulares/main.js
@@ -1,12 +1,58 @@
-import './style.css';
+import './style.css'
 
-const cadena = '394858';
-let expresionRegular = /[01]+/;
-const parrafo = document.createElement('p');
+window.onload = function() {
+  const inputForm = document.getElementById('inputform')
+  // inputForm.addEventListener('submit', getInputValue)
+  inputForm.addEventListener('submit', checkRegularExpresion)
 
-if (expresionRegular.test(cadena)) {
-  parrafo.textContent = 'Si hizo match ✅';
-} else {
-  parrafo.textContent = 'No hizo match 🚩';
+  const clearButton = document.getElementById('clearButton')
+  clearButton.addEventListener('click', clearValues)
 }
-document.body.appendChild(parrafo);
+
+function checkRegularExpresion(e) {
+  e.preventDefault()
+  console.clear()
+  const regexPattern = document.getElementById('regexPattern').value
+  console.log('Patron de la expresion regular: ', regexPattern)
+
+  // const selectRegExFlag = document.getElementById('selectRegExFlag').value
+  // console.log('Bandera selecionada: ', selectRegExFlag)
+
+  let selectedFlags = '';
+
+  selectedFlags += getCheckBoxValue('cbStandar')
+  selectedFlags += getCheckBoxValue('cbGlobal')
+  selectedFlags += getCheckBoxValue('cbCaseInsensitive')
+  console.log('Banderas selecionada: ', selectedFlags)
+
+  let testedString = document.getElementById('testedString').value
+  
+  const expresionRegular = new RegExp(regexPattern, selectedFlags);
+  const results = document.getElementById('results');
+  
+  const concurrencias = testedString.match(expresionRegular)
+  console.log('asdasd');
+
+  if (expresionRegular.test(testedString)){
+    const a = concurrencias.length
+    results.textContent = `Si hizo match!,\nNumero de concurrencias: ${concurrencias.length} \nConcurrencias: ${concurrencias.toString()}`;
+  }
+  else{
+    results.textContent = 'No hizo match. Buuu';
+  }
+}
+
+function getCheckBoxValue(name) {
+  const checkbox = document.getElementById(name)
+  return (checkbox.checked ? checkbox.value : '')
+}
+
+function clearValues() {
+  document.getElementById('regexPattern').value = ""
+  document.getElementById('testedString').value = ""
+  document.getElementById('results').textContent = ""
+  // document.getElementById('selectRegExFlag').value = "g"
+  document.getElementById('cbStandar').checked = false
+  document.getElementById('cbGlobal').checked = true
+  document.getElementById('cbCaseInsensitive').checked = false
+}
